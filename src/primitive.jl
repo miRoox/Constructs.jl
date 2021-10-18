2fdb2f96a1265256b756329d96916b86dc8fbcc5
miroox <miroox@outlook.com>
2021-10-18 23:02:43 +0800

    PrimitiveIO for simple read/write

--- src/primitive.jl
+++ src/primitive.jl
@@ -21,18 +21,22 @@ estimatesize(::Singleton; contextkw...) = 0
 Construct(::Type{Nothing}) = Singleton(nothing)
 Construct(::Type{Missing}) = Singleton(missing)
 
+"""
+    PrimitiveIO{T} <: Construct{T}
+
+Construct based on primitive read/write.
+"""
+struct PrimitiveIO{T} <: Construct{T} end
+
 # primitive numbers
-for type in (Bool, UInt8, UInt16, UInt32, UInt64, UInt128, Int8, Int16, Int32, Int64, Int128, Float16, Float32, Float64)
-    @eval begin
-        Constructs.deserialize(::Default{$type}, s::IO; contextkw...) = Base.read(s, $type)
-        Constructs.serialize(::Default{$type}, s::IO, value::$type; contextkw...) = Base.write(s, value)
-        Constructs.estimatesize(::Default{$type}; contextkw...) = $(Base.sizeof(type))
-    end
+for type in (Bool, Char, UInt8, UInt16, UInt32, UInt64, UInt128, Int8, Int16, Int32, Int64, Int128, Float16, Float32, Float64)
+    @eval Construct(::Type{$type}) = PrimitiveIO{$type}()
 end
 
-deserialize(::Default{Char}, s::IO; contextkw...) = read(s, Char)
-serialize(::Default{Char}, s::IO, c::Char; contextkw...) = write(s ,c)
-estimatesize(::Default{Char}; contextkw...) = Interval{UInt}(1, 4)
+deserialize(::PrimitiveIO{T}, s::IO; contextkw...) where {T} = read(s, T)
+serialize(::PrimitiveIO{T}, s::IO, c::T; contextkw...) where {T} = write(s ,c)
+estimatesize(::PrimitiveIO{T}; contextkw...) where {T} = sizeof(T)
+estimatesize(::PrimitiveIO{Char}; contextkw...) = Interval{UInt}(1, 4)
 
 """
     JuliaSerializer <: Construct{Any}
