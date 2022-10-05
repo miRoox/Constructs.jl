36411dbd44abe97911e12831aa35d14d124d8528
miRoox <miroox@outlook.com>
2022-10-05 23:19:06 +0800

    RaiseError: support UndefProperty

--- src/Error.jl
+++ src/Error.jl
@@ -13,6 +13,7 @@ RaiseError(msg::AbstractString) = RaiseError(ErrorException(msg))
 # this method is not consistent with the Construct interface,
 # but it's acceptable because it never writes anything to the stream.
 serialize(cons::RaiseError, ::IO, ::Any; contextkw...) = throw(cons.err)
+serialize(cons::RaiseError, ::IO, ::UndefProperty; contextkw...) = throw(cons.err)
 # serialize(cons::RaiseError, ::IO, ::Union{}; contextkw...) = throw(cons.err) # unreachable
 deserialize(cons::RaiseError, ::IO; contextkw...) = throw(cons.err)
 estimatesize(cons::RaiseError; contextkw...) = ExactSize(0)
