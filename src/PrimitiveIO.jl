20e0a19f865a9cb2e9b835b291a1bf573c6ac952
miRoox <miroox@outlook.com>
2022-10-08 21:53:49 +0800

    docs: the default types to PrimitiveIO

--- src/PrimitiveIO.jl
+++ src/PrimitiveIO.jl
@@ -1,12 +1,17 @@
+const primitive_types = (Bool, Char, UInt8, UInt16, UInt32, UInt64, UInt128, Int8, Int16, Int32, Int64, Int128, Float16, Float32, Float64)
+const primitive_types_indoc = join(map(t -> string("`", t, "`"), primitive_types), ", ", " and ")
+
 """
     PrimitiveIO{T} <: Construct{T}
 
 Construct based on primitive [`read`](https://docs.julialang.org/en/v1.6/base/io-network/#Base.read)/[`write`](https://docs.julialang.org/en/v1.6/base/io-network/#Base.write).
+
+This is the default construct for $primitive_types_indoc.
 """
 struct PrimitiveIO{T} <: Construct{T} end
 
 # primitive numbers
-for type in (Bool, Char, UInt8, UInt16, UInt32, UInt64, UInt128, Int8, Int16, Int32, Int64, Int128, Float16, Float32, Float64)
+for type in primitive_types
     @eval Construct(::Type{$type}) = PrimitiveIO{$type}()
 end
 
