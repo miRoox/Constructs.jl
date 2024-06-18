ac905bebc124d07a75422a9c2e316eb53fcff20e
Yong-an Lu <miroox@outlook.com>
2024-06-18 21:03:55 +0800

    Draft: Fix tests (#23)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced flexibility in the `Overwrite` function by allowing implicit
    type conversion for the `value` argument.
      
    - **Bug Fixes**
    - Updated type constraints in `Overwrite` constructor to ensure better
    compatibility and error handling.
    
    - **Tests**
    - Adjusted test cases to align with updated type constraints, improving
    overall test accuracy.
      
    - **Refactor**
    - Refined type restrictions in various function calls and declarations
    for improved code clarity and performance.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- test/runtests.jl
+++ test/runtests.jl
@@ -394,8 +394,8 @@ end
     @testset "Overwrite" begin
         @testset "deduce type" begin
             @test Constructs.deducetype((v) -> Overwrite(UInt8, v), UInt8) <: Construct{UInt8}
-            @test Constructs.deducetype((v) -> Overwrite(UInt8, v), Function) <: Construct{UInt8}
-            @test Constructs.deducetype((v) -> Overwrite(UInt8, v), UndefProperty) <: Construct{UInt8}
+            @test Constructs.deducetype((v) -> Overwrite(Int8, v), typeof(abs)) <: Construct{Int8}
+            @test Constructs.deducetype((v) -> Overwrite(UInt8, v), UndefProperty{UInt8}) <: Construct{UInt8}
         end
         @test_throws ArgumentError Overwrite(UInt8, () -> 0x01)
         @test serialize(Overwrite(UInt8, 0x01), 2) == b"\x01"
@@ -643,8 +643,8 @@ end
         withoverwrite = quote
             @construct struct Bitmap <: AbstractImage
                 signature::Const(b"BMP")
-                width::Overwrite(UInt32, convert(UInt32, size(this.pixel, 2)))
-                height::Overwrite(UInt32, convert(UInt32, size(this.pixel, 1)))
+                width::Overwrite(UInt32, _ -> convert(UInt32, size(this.pixel, 2)))
+                height::Overwrite(UInt32, _ -> convert(UInt32, size(this.pixel, 1)))
                 ::Padded(8)
                 pixel::SizedArray(UInt8, this.height, this.width)
             end
@@ -652,8 +652,8 @@ end
         withhidden = quote
             @construct struct Bitmap <: AbstractImage
                 [signature]::Const(b"BMP")
-                [width]::Overwrite(UInt32, convert(UInt32, size(this.pixel, 2)))
-                [height]::Overwrite(UInt32, convert(UInt32, size(this.pixel, 1)))
+                [width]::Overwrite(UInt32, _ -> convert(UInt32, size(this.pixel, 2)))
+                [height]::Overwrite(UInt32, _ -> convert(UInt32, size(this.pixel, 1)))
                 ::Padded(8)
                 pixel::SizedArray(UInt8, this.height, this.width)
             end
